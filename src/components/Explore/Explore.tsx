75b6714fc3145b0916baf8311412170cc44387bf
AdminAkai <akatoirotech@gmail.com>
2023-11-14 14:59:48 -0500

    reverted border radius variable back to static pixel

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -5,11 +5,13 @@ import {
   ExploreSubtitle,
   ExploreTitle,
 } from './styledComponents'
+import CustomButton from 'src/shared/components/CustomButton'
 
 const Explore: FC = () => (
   <ExploreContainer>
     <ExploreSubtitle>Explore</ExploreSubtitle>
     <ExploreTitle>Filipino Cuisine</ExploreTitle>
+    <CustomButton>Browse Menu</CustomButton>
   </ExploreContainer>
 )
 
